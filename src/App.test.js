e4c9ef8ec08c8c3965ca69c3c8a9b55c7bb219a2
Eileen Rhodes <69407155+etrhodes@users.noreply.github.com>
2020-11-29 14:37:19 -0700

    app tests

--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import renderer from 'react-test-renderer';
+import Card from './Card';
+import List from './List';
+import STORE from './store';
+
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+
+
+describe('Card', () => {
+  it('renders the UI as expected', () => {
+    const tree = renderer
+    const div = document.createElement('div');
+    ReactDOM.render(<Card />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  })
+  it('renders the UI as expected', () => {
+    const tree = renderer
+      .create(<Card title="Hello" />)
+      .toJSON();
+      expect(tree).toMatchSnapshot();
+  });
+});
+
+
+describe('List', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<List />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  })
+  it('renders the UI as expected', () => {
+    const tree = renderer
+      .create(<List header="header" />)
+      .toJSON();
+      expect(tree).toMatchSnapshot();
+  });
+})
+
